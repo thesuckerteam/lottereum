0e4c51b67f275b08822863eac2863f0043174b0e
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2020-04-26 00:16:12 +0700

    init web3

--- src/components/content/ContentComponent.js
+++ src/components/content/ContentComponent.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import { Column, Row } from "simple-flexbox";
 import { StyleSheet, css } from "aphrodite/no-important";
 import MiniCardComponent from "./MiniCardComponent";
 import TodayTrendsComponent from "./UserLists";
+import web3 from "../../web3";
+import lottery from "../../lottery";
+import contractInfo from "../../config/contract";
 
 const styles = StyleSheet.create({
 	cardsContainer: {
@@ -43,72 +46,103 @@ const styles = StyleSheet.create({
 	},
 });
 
-function ContentComponent() {
-	return (
-		<Column>
-			<Row
-				className={css(styles.cardsContainer)}
-				wrap
-				flexGrow={1}
-				horizontal='space-between'
-				breakpoints={{ 768: "column" }}>
+export default class ContentComponent extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			manager: "",
+			players: [],
+			balance: "",
+			balanceEther: "",
+			value: "",
+			message: "",
+			isMetaMaskPluginAvailable: false,
+			isTransactionIsRunning: false,
+			startWarning: false,
+			errorMessage: "",
+		};
+	}
+
+	async componentDidMount() {
+		const isMetaMaskPluginAvailable = web3 && lottery;
+		this.setState({ isMetaMaskPluginAvailable });
+		this.updateContractInfo();
+	}
+
+	async updateContractInfo() {
+		fetch("/api/contract-info")
+			.then((res) => res.json())
+			.then(({ manager, players, balanceWei: balance, balanceEther }) => {
+				this.setState({ manager, players, balance, balanceEther });
+			})
+			.catch(console.log);
+	}
+
+	render() {
+		return (
+			<Column>
 				<Row
-					className={css(styles.cardRow)}
+					className={css(styles.cardsContainer)}
 					wrap
 					flexGrow={1}
 					horizontal='space-between'
-					breakpoints={{ 384: "column" }}>
-					<MiniCardComponent
-						className={css(styles.miniCardContainer)}
-						title='first price'
-						value='150 ether'
-					/>
-					<MiniCardComponent
-						className={css(styles.miniCardContainer)}
-						title='Second price'
-						value='120 ether'
-					/>
-					<MiniCardComponent
-						className={css(styles.miniCardContainer)}
-						title='Third price'
-						value='90 ether'
-					/>
-					<MiniCardComponent
-						className={css(styles.miniCardContainer)}
-						title='Forth price'
-						value='60 ether'
-					/>
-					<MiniCardComponent
-						className={css(styles.miniCardContainer)}
-						title='5-30'
-						value='30 ether'
-					/>
-					<MiniCardComponent
-						className={css(styles.miniCardContainer)}
-						title='31-100'
-						value='10 ether'
-					/>
-					<MiniCardComponent
-						className={css(styles.miniCardContainer)}
-						title='3 digits'
-						value='5 ether'
-					/>
-					<MiniCardComponent
-						className={css(styles.miniCardContainer)}
-						title='2 digits'
-						value='1 ether'
-					/>
+					breakpoints={{ 768: "column" }}>
+					<Row
+						className={css(styles.cardRow)}
+						wrap
+						flexGrow={1}
+						horizontal='space-between'
+						breakpoints={{ 384: "column" }}>
+						<MiniCardComponent
+							className={css(styles.miniCardContainer)}
+							title='first price'
+							value='150 ether'
+						/>
+						<MiniCardComponent
+							className={css(styles.miniCardContainer)}
+							title='Second price'
+							value='120 ether'
+						/>
+						<MiniCardComponent
+							className={css(styles.miniCardContainer)}
+							title='Third price'
+							value='90 ether'
+						/>
+						<MiniCardComponent
+							className={css(styles.miniCardContainer)}
+							title='Forth price'
+							value='60 ether'
+						/>
+						<MiniCardComponent
+							className={css(styles.miniCardContainer)}
+							title='5-30'
+							value='30 ether'
+						/>
+						<MiniCardComponent
+							className={css(styles.miniCardContainer)}
+							title='31-100'
+							value='10 ether'
+						/>
+						<MiniCardComponent
+							className={css(styles.miniCardContainer)}
+							title='3 digits'
+							value='5 ether'
+						/>
+						<MiniCardComponent
+							className={css(styles.miniCardContainer)}
+							title='2 digits'
+							value='1 ether'
+						/>
+					</Row>
 				</Row>
-			</Row>
-			<div className={css(styles.todayTrends)}>
-				<TodayTrendsComponent />
-			</div>
-			<Row
-				horizontal='space-between'
-				className={css(styles.lastRow)}
-				breakpoints={{ 1024: "column" }}></Row>
-		</Column>
-	);
+				<div className={css(styles.todayTrends)}>
+					<TodayTrendsComponent />
+				</div>
+				<Row
+					horizontal='space-between'
+					className={css(styles.lastRow)}
+					breakpoints={{ 1024: "column" }}></Row>
+			</Column>
+		);
+	}
 }
-
-export default ContentComponent;
