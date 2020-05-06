1d5ff83abc82b1edf1bde1d1e7276ca57b6a8226
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2020-05-06 19:26:40 +0700

    cover loading overlay

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { StyleSheet, css } from "aphrodite";
 import HeaderSection from "./components/header/HeaderSection";
 import ContentContainer from "./components/content/ContentContainer";
 import "./App.css";
+import LoadingOverlay from "react-loading-overlay";
 
 const styles = StyleSheet.create({
 	container: {
@@ -19,8 +20,15 @@ const styles = StyleSheet.create({
 	},
 });
 
-class App extends React.Component {
-	state = { selectedItem: "Tickets" };
+export default class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			selectedItem: "Tickets",
+			message: "",
+			isTransactionIsRunning: false,
+		};
+	}
 
 	componentDidMount() {
 		window.addEventListener("resize", this.resize);
@@ -30,21 +38,37 @@ class App extends React.Component {
 		window.removeEventListener("resize", this.resize);
 	}
 
+	handleLoading = () => {
+		this.setState({
+			message: "Transaction is processing. This might take a while.",
+			isTransactionIsRunning: true,
+		});
+	};
+
+	handleClose = () => {
+		this.setState({
+			isTransactionIsRunning: false,
+		});
+	};
+
 	resize = () => this.forceUpdate();
 
 	render() {
-		const { selectedItem } = this.state;
+		const { selectedItem, message, isTransactionIsRunning } = this.state;
 		return (
-			<Row className={css(styles.container)}>
-				<Column flexGrow={1} className={css(styles.mainBlock)}>
-					<HeaderSection title={selectedItem} />
-					<div className={css(styles.content)}>
-						<ContentContainer />
-					</div>
-				</Column>
-			</Row>
+			<LoadingOverlay active={isTransactionIsRunning} spinner text={message}>
+				<Row className={css(styles.container)}>
+					<Column flexGrow={1} className={css(styles.mainBlock)}>
+						<HeaderSection title={selectedItem} />
+						<div className={css(styles.content)}>
+							<ContentContainer
+								onLoading={this.handleLoading}
+								onClose={this.handleClose}
+							/>
+						</div>
+					</Column>
+				</Row>
+			</LoadingOverlay>
 		);
 	}
 }
-
-export default App;
