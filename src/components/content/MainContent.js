a9b29d5ce31ffc0db6d9a00b31b4b399a6527973
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2020-05-26 16:36:35 +0700

    fix bug metamask not found

--- src/components/content/MainContent.js
+++ src/components/content/MainContent.js
@@ -8,336 +8,328 @@ import RewardContent from "./RewardContent";
 import lotto from "../../images/lottery_pic.png";
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#FFFFFF",
-    border: "1px solid #DFE0EB",
-    borderRadius: 4,
-    cursor: "pointer",
-    marginTop: 15,
-  },
-  graphSection: {
-	padding: "10px 10px 10px 10px"
-  },
-  separator: {
-    backgroundColor: "#DFE0EB",
-    width: 1,
-    minWidth: 1,
-  },
-  stats: {
-    borderTop: "1px solid #DFE0EB",
-    width: "100%",
-    height: 200,
-    overflowX: "scroll",
-  },
-  todayTrends: {
-    marginTop: 5,
-  },
-  lastRow: {
-    marginTop: 30,
-  },
-  input2: {
-    marginLeft: 10,
-    marginTop: -10,
-    padding: "5px 10px 5px 10px",
-    fontFamily: "Trebuchet MS",
-    fontSize: 15,
-    color: "#fff",
-    fontWeight: 700,
-    backgroundColor: "#00AD5F",
-    borderRadius: 5,
-    opacity: 0.7,
-    transition: 0.3,
-    display: "inline-block",
-    cursor: "pointer",
-    textDecoration: "none",
-    ":hover": { opacity: 1 },
-  },
-  input1: {
-    marginLeft: 10,
-    fontFamily: "Trebuchet MS",
-    padding: "5px 10px 5px 10px",
-    fontSize: 15,
-    color: "#fff",
-    fontWeight: 700,
-    backgroundColor: "#00AD5F",
-    borderRadius: 5,
-    opacity: 0.7,
-    transition: 0.3,
-    display: "inline-block",
-    cursor: "pointer",
-    textDecoration: "none",
-    ":hover": { opacity: 1 },
-  },
-  whiteInput: {
-    fontFamily: "Trebuchet MS",
-    marginLeft: 10,
-    transition: "all 0.2s",
-    fontSize: 15,
-    padding: "5px 0px 5px 10px",
+	container: {
+		backgroundColor: "#FFFFFF",
+		border: "1px solid #DFE0EB",
+		borderRadius: 4,
+		cursor: "pointer",
+		marginTop: 15,
+	},
+	graphSection: {
+		padding: "10px 10px 10px 10px",
+	},
+	separator: {
+		backgroundColor: "#DFE0EB",
+		width: 1,
+		minWidth: 1,
+	},
+	stats: {
+		borderTop: "1px solid #DFE0EB",
+		width: "100%",
+		height: 200,
+		overflowX: "scroll",
+	},
+	todayTrends: {
+		marginTop: 5,
+	},
+	lastRow: {
+		marginTop: 30,
+	},
+	input2: {
+		marginLeft: 10,
+		marginTop: -10,
+		padding: "5px 10px 5px 10px",
+		fontFamily: "Trebuchet MS",
+		fontSize: 15,
+		color: "#fff",
+		fontWeight: 700,
+		backgroundColor: "#00AD5F",
+		borderRadius: 5,
+		opacity: 0.7,
+		transition: 0.3,
+		display: "inline-block",
+		cursor: "pointer",
+		textDecoration: "none",
+		":hover": { opacity: 1 },
+	},
+	input1: {
+		marginLeft: 10,
+		fontFamily: "Trebuchet MS",
+		padding: "5px 10px 5px 10px",
+		fontSize: 15,
+		color: "#fff",
+		fontWeight: 700,
+		backgroundColor: "#00AD5F",
+		borderRadius: 5,
+		opacity: 0.7,
+		transition: 0.3,
+		display: "inline-block",
+		cursor: "pointer",
+		textDecoration: "none",
+		":hover": { opacity: 1 },
+	},
+	whiteInput: {
+		fontFamily: "Trebuchet MS",
+		marginLeft: 10,
+		transition: "all 0.2s",
+		fontSize: 15,
+		padding: "5px 0px 5px 10px",
 
-    ":hover": {
-      background: "#efefef",
-    },
-  },
+		":hover": {
+			background: "#efefef",
+		},
+	},
 
-  playerContain: {
-    overflowX: "auto",
-    height: 350,
-    width: "500px",
-    float: "left",
-    position: "relative",
-  },
-  rowContain: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  imageSet:{
-	bottom: 0,
-	width: '100%',
-	height: 280,
-	objectFit: 'cover'
-	  
-  }
+	playerContain: {
+		overflowX: "auto",
+		height: 350,
+		width: "500px",
+		float: "left",
+		position: "relative",
+	},
+	rowContain: {
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	imageSet: {
+		bottom: 0,
+		width: "100%",
+		height: 280,
+		objectFit: "cover",
+	},
 });
 
 export default class MainContent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      manager: "",
-      players: [],
-      balance: "",
-      balanceEther: "",
-      value: "",
-      isMetaMaskPluginAvailable: false,
-      isWarningAppended: false,
-      errorMessage: "",
-      isLoading: true,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			manager: "",
+			players: [],
+			balance: "",
+			balanceEther: "",
+			value: "",
+			isMetaMaskPluginAvailable: false,
+			isWarningAppended: false,
+			errorMessage: "",
+			isLoading: true,
+		};
+	}
 
-  async componentDidMount() {
-    const isMetaMaskPluginAvailable = web3 && lottery;
-    this.setState({ isMetaMaskPluginAvailable, isLoading: true });
-    console.log("Account: ", await web3.eth.getAccounts());
-    this.updateContractInfo();
-  }
+	async componentDidMount() {
+		const isMetaMaskPluginAvailable = web3 && lottery;
+		this.setState({ isMetaMaskPluginAvailable, isLoading: true });
+		console.log("Account: ", await web3.eth.getAccounts());
+		this.updateContractInfo();
+	}
 
-  updateContractInfo = async () => {
-    try {
-      const manager = await lottery.methods.manager().call();
-      const players = await lottery.methods.getPlayers().call();
-      const balanceWei = await web3.eth.getBalance(lottery.options.address);
-      const balanceEther = await web3.utils.fromWei(balanceWei, "ether");
-      this.setState({
-        manager,
-        players,
-        balance: balanceWei,
-        balanceEther,
-        isLoading: false,
-      });
-    } catch (error) {
-      alert(error);
-    }
-  };
+	updateContractInfo = async () => {
+		try {
+			const manager = await lottery.methods.manager().call();
+			const players = await lottery.methods.getPlayers().call();
+			const balanceWei = await web3.eth.getBalance(lottery.options.address);
+			const balanceEther = await web3.utils.fromWei(balanceWei, "ether");
+			this.setState({
+				manager,
+				players,
+				balance: balanceWei,
+				balanceEther,
+				isLoading: false,
+			});
+		} catch (error) {
+			alert(error);
+		}
+	};
 
-  handleInputChange = (event) => {
-    const value = event.target.value;
+	handleInputChange = (event) => {
+		const value = event.target.value;
 
-    if (!value || value.match(/^\d{1,}(\.\d{0,4})?$/)) {
-      this.setState(() => ({ value }));
-    }
-  };
+		if (!value || value.match(/^\d{1,}(\.\d{0,4})?$/)) {
+			this.setState(() => ({ value }));
+		}
+	};
 
-  handleOnSubmit = async (event) => {
-    event.preventDefault();
-    this.setState({ errorMessage: "" });
-    const { isMetaMaskPluginAvailable, value } = this.state;
+	handleOnSubmit = async (event) => {
+		event.preventDefault();
+		this.setState({ errorMessage: "" });
+		const { isMetaMaskPluginAvailable, value } = this.state;
 
-    if (!isMetaMaskPluginAvailable) {
-      return this.metaMaskNotAvailable();
-    }
+		if (!isMetaMaskPluginAvailable) {
+			return this.metaMaskNotAvailable();
+		}
 
-    await window.ethereum.enable();
-    const accounts = await web3.eth.getAccounts();
-    this.props.onLoading();
+		this.props.onLoading();
 
-    try {
-      await lottery.methods.enter().send({
-        from: accounts[0],
-        value: web3.utils.toWei(value, "ether"),
-      });
-      this.updateContractInfo();
-      this.setState({
-        value: "",
-      });
-    } catch (err) {
-      this.setState({
-        errorMessage: err.message,
-      });
-    }
-    this.props.onClose();
-    this.setState({ isLoading: false });
-  };
+		try {
+			await window.ethereum.enable();
+			const accounts = await web3.eth.getAccounts();
+			await lottery.methods.enter().send({
+				from: accounts[0],
+				value: web3.utils.toWei(value, "ether"),
+			});
+			this.updateContractInfo();
+			this.setState({
+				value: "",
+			});
+		} catch (error) {
+			this.setState({
+				errorMessage: error.message,
+				isWarningAppended: true,
+			});
+		}
+		this.props.onClose();
+		this.setState({ isLoading: false });
+	};
 
-  handleOnPickWinner = async (event) => {
-    event.preventDefault();
+	handleOnPickWinner = async (event) => {
+		event.preventDefault();
 
-    const { isMetaMaskPluginAvailable, balanceEther } = this.state;
-    if (!isMetaMaskPluginAvailable) {
-      return this.metaMaskNotAvailable();
-    }
+		const { isMetaMaskPluginAvailable, balanceEther } = this.state;
+		if (!isMetaMaskPluginAvailable) {
+			return this.metaMaskNotAvailable();
+		}
 
-    await window.ethereum.enable();
-    const accounts = await web3.eth.getAccounts();
-    this.props.onLoading();
+		this.props.onLoading();
 
-    try {
-      await lottery.methods.pickWinner().send({
-        from: accounts[0],
-      });
-    } catch (ex) {}
-    const winner = await lottery.methods.winner().call();
-    alert(`The winner is ${winner}, wins ${balanceEther} ether`);
-    this.props.onClose();
-    this.setState({ balanceEther: 0 });
-  };
+		try {
+			await window.ethereum.enable();
+			const accounts = await web3.eth.getAccounts();
+			await lottery.methods.pickWinner().send({
+				from: accounts[0],
+			});
+		} catch (error) {
+			this.setState({ isWarningAppended: true });
+		}
+		const winner = await lottery.methods.winner().call();
+		alert(`The winner is ${winner}, wins ${balanceEther} ether`);
+		this.props.onClose();
+		this.setState({ balanceEther: 0 });
+	};
 
-  metaMaskNotAvailable = () => {
-    this.setState({
-      isWarningAppended: true,
-    });
-  };
+	metaMaskNotAvailable = () => {
+		this.metaMaskNotAvailable();
+		this.setState({
+			isWarningAppended: true,
+		});
+	};
 
-  renderLegend(color, title) {
-    return (
-      <Row vertical="center">
-        <div
-          style={{ width: 16, border: "2px solid", borderColor: color }}
-        ></div>
-        <span className={css(styles.legendTitle)}>{title}</span>
-      </Row>
-    );
-  }
+	renderLegend(color, title) {
+		return (
+			<Row vertical='center'>
+				<div
+					style={{ width: 16, border: "2px solid", borderColor: color }}></div>
+				<span className={css(styles.legendTitle)}>{title}</span>
+			</Row>
+		);
+	}
 
-  render() {
-    const {
-      value,
-      isMetaMaskPluginAvailable,
-      isWarningAppended,
-      isLoading,
-      manager,
-      players,
-      balance,
-      balanceEther,
-    } = this.state;
+	render() {
+		const {
+			value,
+			isMetaMaskPluginAvailable,
+			isWarningAppended,
+			isLoading,
+			manager,
+			players,
+			balance,
+			balanceEther,
+		} = this.state;
 
-    console.log("Metamask: ", isMetaMaskPluginAvailable);
-    console.log("Manager: ", manager);
-    console.log("Player: ", players);
-    console.log("Balance: ", balance);
-    console.log("BalanceEther: ", balanceEther);
+		console.log("Metamask: ", isMetaMaskPluginAvailable);
+		console.log("Manager: ", manager);
+		console.log("Player: ", players);
+		console.log("Balance: ", balance);
+		console.log("BalanceEther: ", balanceEther);
 
-    return (
-      <Column>
-        {isWarningAppended && <div>Error please enable Metamask!</div>}
-        <RewardContent balanceEther={balanceEther} />
-        <div className={css(styles.todayTrends)}>
-          <Row
-            flexGrow={1}
-            horizontal=""
-            justifyContent="center"
-            alignItems="center"
-          >
-            Total&nbsp;<h3>{players.length}</h3>&nbsp;players already joined
-            here
-          </Row>
-          <Row
-            flexGrow={1}
-            horizontal=""
-            justifyContent="center"
-            alignItems="center"
-          >
-            The contract is managed by {manager} <br></br>
-          </Row>
-          <Row
-            flexGrow={1}
-            className={css(styles.container)}
-            horizontal="center"
-            breakpoints={{ 1024: "column" }}
-          >
-            <Column
-              wrap
-              alignItems="center"
-              flexGrow={7}
-              flexBasis="735px"
-              className={css(styles.graphSection)}
-              breakpoints={{
-                1024: { width: "calc(100% - 48px)", flexBasis: "auto" },
-              }}
-            >
-              <h2>
-                <form>
-                  <label>
-                    Lottery Amount:
-                    <input
-                      className={css(styles.whiteInput)}
-                      onChange={this.handleInputChange}
-                      placeholder="Example: 0.1"
-                      value={value}
-                    />
-                  </label>
-                  <input
-                    className={css(styles.input1)}
-                    type="submit"
-                    value="Submit"
-                    onClick={this.handleOnSubmit}
-                  />
-                </form>
-              </h2>
-			<img
-			className={css(styles.imageSet)}
-			src={lotto}/>
-            </Column>
-            <Column
-              className={css(styles.separator)}
-              breakpoints={{ 1024: { display: "none" } }}
-            >
-              <div />
-            </Column>
-            <Column
-              className={css(styles.playerContain)}
-              flexGrow={3}
-              flexBasis="342px"
-              breakpoints={{ 1024: css(styles.stats) }}
-            >
-              {isLoading ||
-                players.map((address, index) => (
-                  <PlayerCard number={index + 1} address={address} />
-                ))}
-            </Column>
-          </Row>
-        </div>
-        <Row
-          horizontal="space-between"
-          alignItems="center"
-          className={css(styles.lastRow)}
-          breakpoints={{ 1024: "column" }}
-        ></Row>
-        <Row
-          flexGrow={1}
-          horizontal=""
-          justifyContent="center"
-          alignItems="center"
-        >
-          <input
-            className={css(styles.input2)}
-            type="submit"
-            value="Pick A Winner"
-            onClick={this.handleOnPickWinner}
-          />
-        </Row>
-      </Column>
-    );
-  }
+		return (
+			<Column>
+				{isWarningAppended && <div>Error please enable Metamask!</div>}
+				<RewardContent balanceEther={balanceEther} />
+				<div className={css(styles.todayTrends)}>
+					<Row
+						flexGrow={1}
+						horizontal=''
+						justifyContent='center'
+						alignItems='center'>
+						Total&nbsp;<h3>{players.length}</h3>&nbsp;players already joined
+						here
+					</Row>
+					<Row
+						flexGrow={1}
+						horizontal=''
+						justifyContent='center'
+						alignItems='center'>
+						The contract is managed by {manager} <br></br>
+					</Row>
+					<Row
+						flexGrow={1}
+						className={css(styles.container)}
+						horizontal='center'
+						breakpoints={{ 1024: "column" }}>
+						<Column
+							wrap
+							alignItems='center'
+							flexGrow={7}
+							flexBasis='735px'
+							className={css(styles.graphSection)}
+							breakpoints={{
+								1024: { width: "calc(100% - 48px)", flexBasis: "auto" },
+							}}>
+							<h2>
+								<form>
+									<label>
+										Lottery Amount:
+										<input
+											className={css(styles.whiteInput)}
+											onChange={this.handleInputChange}
+											placeholder='Example: 0.1'
+											value={value}
+										/>
+									</label>
+									<input
+										className={css(styles.input1)}
+										type='submit'
+										value='Submit'
+										onClick={this.handleOnSubmit}
+									/>
+								</form>
+							</h2>
+							<img className={css(styles.imageSet)} src={lotto} />
+						</Column>
+						<Column
+							className={css(styles.separator)}
+							breakpoints={{ 1024: { display: "none" } }}>
+							<div />
+						</Column>
+						<Column
+							className={css(styles.playerContain)}
+							flexGrow={3}
+							flexBasis='342px'
+							breakpoints={{ 1024: css(styles.stats) }}>
+							{isLoading ||
+								players.map((address, index) => (
+									<PlayerCard number={index + 1} address={address} />
+								))}
+						</Column>
+					</Row>
+				</div>
+				<Row
+					horizontal='space-between'
+					alignItems='center'
+					className={css(styles.lastRow)}
+					breakpoints={{ 1024: "column" }}></Row>
+				<Row
+					flexGrow={1}
+					horizontal=''
+					justifyContent='center'
+					alignItems='center'>
+					<input
+						className={css(styles.input2)}
+						type='submit'
+						value='Pick A Winner'
+						onClick={this.handleOnPickWinner}
+					/>
+				</Row>
+			</Column>
+		);
+	}
 }
